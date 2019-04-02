c00b80b2342023198a9831c58e5fc31856fcb0f7
Kage18 <18kushaljain@gmail.com>
2019-04-02 23:10:03 +0530

    groups sql_queries

--- eventManagement/groups/views.py
+++ eventManagement/groups/views.py
@@ -12,16 +12,21 @@ def groupview(request):
     if request.POST:
         form = GroupsForm(request.POST)
         if form.is_valid():
-            group = form.save(request)
+            # group = form.save(request)
             with connection.cursor() as cursor:
-            # event = form.save(request)
-                cursor.execute("INSERT INTO events_event (date,description,time,city,state,private,venue,name,user_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", [form.cleaned_data['date'], form.cleaned_data['description'], (str)(time), form.cleaned_data['city'], form.cleaned_data['state'],form.cleaned_data['private'], form.cleaned_data['venue'], form.cleaned_data['name'], request.user.id])
-                eid = cursor.lastrowid
+                cursor.execute("INSERT INTO groups_group(name, description, creator_id) VALUES (%s,%s,%s)",
+                               [form.cleaned_data['name'], form.cleaned_data['description'], request.user.id])
+                gid = cursor.lastrowid
                 cursor.close()
+            for k in form.cleaned_data['members']:
+                with connection.cursor() as cursor:
+                    cursor.execute("INSERT INTO groups_group_members(group_id,user_id) VALUES (%s,%s)", [(str)(gid), k.id])
+                    cursor.close()
             for k in form.cleaned_data['to']:
-                cursor = connection.cursor()
-                cursor.execute("INSERT INTO groups_group_invite (group_id,to_id,status) VALUES( %s , %s , %s)", [group.id, k, 'False'])
-                cursor.close()
+                with connection.cursor() as cursor:
+                    cursor.execute("INSERT INTO groups_group_invite (group_id,to_id,status) VALUES( %s , %s , %s)",
+                                   [(str)(gid), k, 'False'])
+                    cursor.close()
                 # user = User.objects.get(id=k)
                 # print('user: ', user.query)
                 # invite = Group_invite.objects.create(to=user)
@@ -37,11 +42,15 @@ def groupview(request):
 @login_required(login_url="/accounts/login")
 def accept_invite(request, pk):
     if pk:
-        invite = Group_invite.objects.get(id=pk)
-        invite.status = True
-        # thisevent=event.objects.get(id=invite.event.id)
-        invite.group.members.add(invite.to)
-        invite.save()
-
+        # invite = Group_invite.objects.get(id=pk)
+        # invite.status = True
+        # # thisevent=event.objects.get(id=invite.event.id)
+        # invite.group.members.add(invite.to)
+        # invite.save()
+        with connection.cursor() as cursor:
+            cursor.execute("UPDATE groups_group_invite SET status = %s WHERE id = %s", [1, pk])
+            group = Group_invite.objects.raw('select * from groups_group_invite where id = %s', [pk])[0]
+            cursor.execute("INSERT INTO groups_group_members (group_id, user_id) VALUES( %s , %s )", [group.group_id, group.to_id])
+            cursor.close()
     return redirect(reverse('accounts:home'))
 
