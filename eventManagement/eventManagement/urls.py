e300413d804f107d9102d5bd1f750a61ce0c6750
Kage18 <18kushaljain@gmail.com>
2019-05-01 08:19:58 +0530

    integration2

--- eventManagement/eventManagement/urls.py
+++ eventManagement/eventManagement/urls.py
@@ -43,4 +43,5 @@ urlpatterns = [
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-activity.userActivity(repeat=604800,repeat_until=None)
+# activity.userActivity(repeat=604800,repeat_until=None)
+# activity.userActivity(repeat=300,repeat_until=None)
